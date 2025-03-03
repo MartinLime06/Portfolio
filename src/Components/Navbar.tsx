48ae52cee0be0df52cdaecdd9fc6cec6c7963d4d
Martin Tangen <tangenmartin06@gmail.com>
2025-03-03 19:08:23 +0100

    new ui changes to black and white

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-scroll';
 
 function Navbar() {
     return (
-        <div className="flex justify-end items-center scroll-smooth animate-ease-out">
-          <div className='flex justify-center items-center px-10 py-5 rounded-lg gap-5'>
+        <div className="flex justify-center items-center scroll-smooth animate-ease-out p-10">
+          <div className='fixed top-10 bg-black items-center px-10 py-5 rounded-full border-1 border-gray-500/10 shadow-sm shadow-neutral-500 z-100 gap-5'>
             <button><Link to='header' className='hover:opacity-75 cursor-pointer'>Hjem</Link></button>
             <button><Link to='about' className='hover:opacity-75 cursor-pointer'>Om Meg</Link></button>
             <button><Link to='work' className='hover:opacity-75 cursor-pointer'>Arbeid</Link></button>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -11,12 +11,15 @@ import Services from '../Components/Services';
 
 const Home: React.FC = () => {
     return (
-        <div>
+        <div className='bg-black text-white'>
+            <div className='absolute blur-lg h-screen w-screen bg-black-100'>
+                <div className='absolute bg-white h-30 w-400 rotate-40 opacity-20 rounded-full shadow-xl shadow-white'></div></div>
             <Navbar />
-            <div className='flex flex-col justify-center items-center h-screen' id='header'>
+            <div className='flex flex-col justify-center items-center h-[50vh]' id='header'>
                 <div className='flex flex-col items-center'>
                     <p className=''>BASED IN NORWAY</p>
-                    <h1 className='text-7xl font-bold p-5'>Junior <span className='text-sky-500'>Web Developer</span></h1>
+                    <h1 className='text-7xl font-bold p-2'>Martin Tangen</h1>
+                    <h1 className='text-7xl font-bold p-2'>Junior <span className='text-sky-500'>Web Developer</span></h1>
                     <p className='text-zinc-500'>Hei jeg er Martin Tangen, jeg lager høyt funksjonele nettsider og web apper</p>
                 </div>
                 <div className='m-5'>
