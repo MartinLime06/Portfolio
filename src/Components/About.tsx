14a4125a31302bc1c66e6576aca822760a8b76da
Martin_Lime <57748362+MartinLime06@users.noreply.github.com>
2025-03-04 07:47:10 +0100

    new responsive header and work

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -2,8 +2,8 @@ function About() {
     return (
       <div id="about" className="flex justify-center items-center flex-col gap-5">
         <div className="flex flex-col items-left lg:w-3/5">
-          <h1 className="font-sans text-5xl font-bold">Teknologi</h1>
-          <p className="opacity-75 mt-2">
+          <h1 className="font-sans text-2xl lg:text-5xl font-bold">Teknologi</h1>
+          <p className="opacity-75 mt-2 text-sm lg:text-base">
             Under er en liste over kodespråk og andre apper jeg har erfaring med
           </p>
         </div>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -17,13 +17,13 @@ const Home: React.FC = () => {
             <div className='flex flex-col justify-center items-center h-[50vh] z-50' id='header'>
                 <div className='flex flex-col items-center'>
                     <p className=''>BASED IN NORWAY</p>
-                    <h1 className='text-7xl font-bold p-2'>Martin Tangen</h1>
-                    <h1 className='text-7xl font-bold p-2 mb-2'>Junior <span className='text-sky-500'>Web Developer</span></h1>
-                    <p className='text-zinc-500'>Hei jeg er Martin Tangen, jeg lager høyt funksjonele nettsider og web apper</p>
+                    <h1 className='text-3xl lg:text-7xl font-bold p-2'>Martin Tangen</h1>
+                    <h1 className='text-3xl lg:text-7xl font-bold p-2 mb-2'>Junior <span className='text-sky-500'>Web Developer</span></h1>
+                    <p className='text-sm lg:text-xl text-zinc-500'>Hei jeg er Martin Tangen, jeg lager høyt funksjonele nettsider og web apper</p>
                 </div>
                 <div className='m-5'>
-                    <Link to='work'><button className='bg-sky-500/40 px-10 py-4 rounded-lg cursor-pointer hover:bg-sky-800'>Mitt Arbeid  <FontAwesomeIcon icon={faChevronRight} /></button></Link>
-                    <a href='/CV.docx' className='my-4 mx-10 cursor-pointer hover:opacity-75'><FontAwesomeIcon icon={faDownload} className='text-sky-500'/> Last Ned CV</a>
+                    <Link to='work'><button className='bg-sky-500/40 px-10 py-4 rounded-lg cursor-pointer hover:bg-sky-800 text-sm lg:text-base'>Mitt Arbeid  <FontAwesomeIcon icon={faChevronRight} /></button></Link>
+                    <a href='/CV.docx' className='my-4 mx-10 cursor-pointer hover:opacity-75 text-sm lg:text-base'><FontAwesomeIcon icon={faDownload} className='text-sky-500'/> Last Ned CV</a>
                 </div>
             </div>
             <About />
